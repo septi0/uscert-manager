84a472d5f451aae5cc7fe8c8d35690148b56dc8d
Septi <septimiu.ujica@gmail.com>
2026-05-06 20:06:41 +0300

    added Dockerfile

--- setup.py
+++ setup.py
@@ -14,19 +14,19 @@ with open("README.md", "r") as f:
     long_description = f.read()
 
 setup(
-    name=info['__package_name__'],
+    name=info["__package_name__"],
     version=version,
-    description=info['__description__'],
+    description=info["__description__"],
     long_description=long_description,
     long_description_content_type="text/markdown",
-    license=info['__license__'],
-    author=info['__author__'],
-    author_email=info['__author_email__'],
-    author_url=info['__author_url__'],
-    python_requires='>=3.9',
+    license=info["__license__"],
+    author=info["__author__"],
+    author_email=info["__author_email__"],
+    author_url=info["__author_url__"],
+    python_requires=">=3.9",
     packages=[
-        'uscert_manager',
-        'uscert_manager.providers',
+        "uscert_manager",
+        "uscert_manager.providers",
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
@@ -37,9 +37,14 @@ setup(
         "Topic :: Utilities",
     ],
     entry_points={
-        'console_scripts': [
-            'uscert-manager = uscert_manager:main',
+        "console_scripts": [
+            "uscert-manager = uscert_manager:main",
         ],
     },
-    options={}
+    include_package_data=True,
+    package_data={
+        "uscert_manager": [
+            "VERSION",
+        ],
+    },
 )
